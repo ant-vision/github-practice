19a38b196e889ef60c3123356623cc2b8837eaa1
hooyuCat <hooyu1072@gmail.com>
2019-09-09 16:13:38 +0800

    喵喵喵？

---
+++ git-demo-core/src/main/java/com/yinhai/MiaoWu.java
@@ -0,0 +1,15 @@
+package com.yinhai;
+
+/**
+ * @version 1.0
+ * @ClassName: git-demo
+ * @Description: 喵呜
+ * @Company: com.yinhai
+ * @author: houyu
+ * @date: 2019-09-09 16:09
+ **/
+public class MiaoWu {
+    public static void main(String[] args) {
+        System.out.println("喵喵喵~~~");
+    }
+}
