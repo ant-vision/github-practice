ea77257528a38243c95a0ddf1b17e1ac74c350cb
ant-vision <vision311@163.com>
2019-09-09 11:36:39 +0800

    新增代码练习

---
+++ git-demo-core/src/main/java/com/yinhai/Test.java
@@ -0,0 +1,13 @@
+package com.yinhai;
+
+public class Test {
+    public static void main(String[] args) {
+        // 测试冲突  1212312
+        System.out.println("测试冲突");
+        System.out.println(11111);
+        System.out.println(22);
+        System.out.println(333);
+        System.out.println(44);
+        System.out.println(55);
+    }
+}
---
+++ git-demo-core/src/main/java/com/yinhai/demo/YinHai.java
@@ -0,0 +1,4 @@
+package com.yinhai.demo;
+
+public class YinHai {
+}
