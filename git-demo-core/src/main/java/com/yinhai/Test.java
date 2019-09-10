623a9ab1161692f2af7883c04e5cbb868f41f150
FengLinYh <672684866@qq.com>
2019-09-10 08:59:22 +0800

    删除66

--- git-demo-core/src/main/java/com/yinhai/Test.java
+++ git-demo-core/src/main/java/com/yinhai/Test.java
@@ -9,7 +9,6 @@ public class Test {
         System.out.println(333);
         System.out.println(44);
         System.out.println(55);
-        System.out.println(66);
         System.out.println("哈哈哈哈");
         System.out.println("吃月饼了哈哈哈哈哈哈");
         System.out.println("冲突111");
